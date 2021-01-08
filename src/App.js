b0c61f0998d664d99080eb4d6bf13c0364ffd494
Ravi <ravi.exe.doc@gmail.com>
2021-01-08 13:10:36 +0530

    first

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://ravi.com"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
